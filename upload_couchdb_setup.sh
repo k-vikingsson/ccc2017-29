798e481745715ab137d263cc5c1945abd563dc2a
Zequn Ma <zequnm@student.unimelb.edu.au>
2017-04-26 18:57:36 +1000

    yet another script

---
+++ upload_couchdb_setup.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+scp -i cloud.key ./couchdb-setup.sh ubuntu@115.146.93.125:
+scp -i cloud.key ./couchdb-setup.sh ubuntu@115.146.93.125:
+scp -i cloud.key ./couchdb-setup.sh ubuntu@115.146.93.125:
+scp -i cloud.key ./couchdb-setup.sh ubuntu@115.146.93.125:
