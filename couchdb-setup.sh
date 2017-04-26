76d644e9916dcdd6426309d210b5dad79dbf288e
Zequn Ma <zequnm@student.unimelb.edu.au>
2017-04-26 18:53:13 +1000

    edited script

--- couchdb-setup.sh
+++ couchdb-setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Place this script in ~/Documents directory.
+# Navigate to home directory
+cd
 
 # Download source
 wget "http://apache.mirror.serversaustralia.com.au/couchdb/source/2.0.0/apache-couchdb-2.0.0.tar.gz"
@@ -10,11 +11,11 @@ tar -xvzf apache-couchdb-2.0.0.tar.gz
 sudo apt-get --no-install-recommends -y install build-essential pkg-config erlang libicu-dev libmozjs185-dev libcurl4-openssl-dev
 
 # Build source
-cd ~/Documents/apache-couchdb-2.0.0/
+cd ~/apache-couchdb-2.0.0/
 ./configure
 make release
 
 # Start couchDB
-cp -r ./rel/couchdb/ ~/Documents/
-# cd ~/Documents/couchdb
+cp -r ./rel/couchdb/ ~/
+# cd ~/couchdb
 # ./bin/couchdb
