08890eff427778bc7bf1766e2b7df72226d9459a
Zequn Ma <zequnm@student.unimelb.edu.au>
2017-04-30 12:28:29 +1000

    twitter spider

--- couchdb-setup.sh
+++ couchdb-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Navigate to home directory
-cd
+cd ~
 
 # Download source
 wget "http://apache.mirror.serversaustralia.com.au/couchdb/source/2.0.0/apache-couchdb-2.0.0.tar.gz"
@@ -15,7 +15,29 @@ cd ~/apache-couchdb-2.0.0/
 ./configure
 make release
 
-# Start couchDB
-cp -r ./rel/couchdb/ ~/
+sudo adduser --system --shell /bin/bash --group --gecos "CouchDB Administrator" couchdb
+
+# Prepare couchDB
+sudo cp -r ./rel/couchdb/ /home/couchdb/
+sudo chown -R couchdb:couchdb /home/couchdb/couchdb
+sudo find /home/couchdb/couchdb -type d -exec chmod 0770 {} \;
+
+# Setup firewall rules
+sudo ufw allow 22/tcp
+sudo ufw enable
+sudo ufw allow 5984/tcp
+sudo ufw allow 4369/tcp
+sudo ufw allow 9100:9200/tcp
+
+#sudo chmod 0644 /home/couchdb/couchdb/etc/*
+
+# Setup cluster
+#sudo apt-get -y install curl
+#sudo curl -X PUT http://localhost:5984/_users
+#sudo curl -X PUT http://localhost:5984/_replicator
+#sudo curl -X PUT http://localhost:5984/_global_changes
+
+# Start CouchDB
+#sudo -i -u couchdb couchdb/bin/couchdb
 # cd ~/couchdb
 # ./bin/couchdb
--- upload_couchdb_setup.sh
+++ upload_couchdb_setup.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 scp -i cloud.key ./couchdb-setup.sh ubuntu@115.146.93.125:
-scp -i cloud.key ./couchdb-setup.sh ubuntu@115.146.93.125:
-scp -i cloud.key ./couchdb-setup.sh ubuntu@115.146.93.125:
-scp -i cloud.key ./couchdb-setup.sh ubuntu@115.146.93.125:
+scp -i cloud.key ./couchdb-setup.sh ubuntu@115.146.93.129:
+scp -i cloud.key ./couchdb-setup.sh ubuntu@115.146.93.132:
+scp -i cloud.key ./couchdb-setup.sh ubuntu@115.146.93.137:
